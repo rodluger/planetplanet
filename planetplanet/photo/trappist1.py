3e137e5818c583a121d1758789431f94032afee7
Rodrigo Luger <rodluger@gmail.com>
2017-06-07 15:56:37 -0700

    misc

--- planetplanet/photo/trappist1.py
+++ planetplanet/photo/trappist1.py
@@ -23,142 +23,6 @@ AUM = 1.49598e11
 
 __all__ = ['Trappist1']
 
-def h(i, Om):
-  '''
-  The angular momentum unit vector given the inclination `i`
-  and the longitude of ascending node `Om`
-  
-  '''
-  
-  hx = np.sin(i) * np.sin(Om)
-  hy = -np.sin(i) * np.cos(Om)
-  hz = np.cos(i)
-  return np.array([hx, hy, hz])
-
-def LnLikelihood(p, imu, isig, imin):
-  '''
-  The likelihood of observing all seven planets transit given
-  a set of angular momentum vectors for each planet and a value for the
-  standard deviation of the distribution of longitude of ascending
-  nodes.
-  
-  '''
-  
-  # Get the parameters; set Om_b = 0
-  sigOm = p[0]
-  x = np.array(p[0::3]); x[0] = 0
-  y = np.array(p[1::3])
-  z = np.array(p[2::3])
-  n = len(x)
-  
-  # Initialize
-  lnlike = 0
-  Om = np.zeros(n) * np.nan
-  Om[0] = sigOm
-  
-  # Enforce positive standard deviation for omega
-  if sigOm < 0:
-    return -np.inf, Om
-    
-  # Loop over all planets
-  for j in range(n):
-    
-    # Enforce normal priors with unit standard deviation on x, y, and z
-    lnlike -= 0.5 * (x[j] ** 2 + y[j] ** 2 + z[j] ** 2)
-    
-    # Normalize to get the unit vector
-    norm = np.sqrt(x[j] ** 2 + y[j] ** 2 + z[j] ** 2)
-    x[j] /= norm
-    z[j] /= norm
-    
-    # Compute the inclination in the range [0-90]
-    i = np.arccos(np.abs(z[j]))
-    
-    # Compute the inclination if the orbit precessed 90 degrees
-    # and reject this sample if the planet doesn't transit
-    i90 = np.arccos(np.abs(x[j]))
-    if i90 < imin[j]:
-      return -np.inf, Om
-    
-    # Probability given the data for either
-    # positive or negative impact parameter
-    # (we pick the highest likelihood one)
-    l1 = 0.5 * ((i - imu[j]) / isig[j]) ** 2
-    l2 = 0.5 * ((i - (np.pi - imu[j])) / isig[j]) ** 2
-    lnlike -= min(l1, l2)
-
-    # Compute omega and its probability given sigOm
-    if j > 0:
-      Om[j] = np.arcsin(x[j] / np.sin(i))
-      lnlike -= 0.5 * (Om[j] / sigOm) ** 2
-      lnlike -= np.log(np.sqrt(2 * np.pi) * sigOm)
-
-  return lnlike, Om
-
-def OmegaPrior(nwalk = 42, nsteps = 50000, nburn = 10000, thin = 10, calc = False):
-  '''
-  Computes the prior standard deviation of the distribution of longitude of ascending
-  nodes for the planets in the TRAPPSIT-1 system. This yields sigma_Om ~ 0.4 degrees.
-  
-  '''
-  
-  # Parameters from Gillon et al. (2017) and Luger et al. (2017)
-  imu = np.array([89.65, 89.670, 89.750, 89.86, 89.680, 89.710, 89.80]) * np.pi / 180
-  isig = np.array([0.245, 0.170, 0.160, 0.110, 0.034, 0.025, 0.075]) * np.pi / 180
-  imin = np.arccos(1. / np.array([20.50, 28.08, 39.55, 51.97, 68.4, 83.2, 109.]))
-  ndim = len(imu) * 3
-  
-  # Initial values
-  def get_guess():
-    x0 = []
-    # Initial values for x, y, and z for each planet, computed from
-    # the observed inclination distribution and assuming the Omegas
-    # are close to zero with standard deviation 0.1 degrees
-    for m,s in zip(imu, isig):
-      if np.random.rand() < 0.5:
-        # Positive impact parameter
-        x, y, z = h(m + s * np.random.randn(), 0.1 * np.random.randn() * np.pi / 180)
-      else:
-        # Negative impact parameter
-        x, y, z = h(np.pi - m + s * np.random.randn(), 0.1 * np.random.randn() * np.pi / 180)
-      x0.extend([x,y,z])
-    # Initial value for sigma omega -- about 0.1 degrees
-    x0[0] = np.abs(0.1 * np.random.randn() * np.pi / 180)
-    return x0
-  x0 = np.array([get_guess() for w in range(nwalk)])
-  _, blobs0 = LnLikelihood(x0[0], imu, isig, imin)
-  
-  # Run the chain
-  if calc or not os.path.exists('omega.npz'):
-    sampler = emcee.EnsembleSampler(nwalk, ndim, LnLikelihood, args = (imu, isig, imin))
-    for i in tqdm(sampler.sample(x0, iterations = nsteps, thin = thin, blobs0 = blobs0), total = nsteps):
-      pass
-    chain = sampler.chain
-    blobs = sampler.blobs
-    np.savez('omega.npz', chain = chain, blobs = blobs)
-  else:
-    data = np.load('omega.npz')
-    blobs = data['blobs']
-    chain = data['chain']
-    
-  # Plot the corner plot, with values in degrees
-  labels = [r'$\sigma_\Omega$'] + [r'$\Omega_\mathrm{%s}$' % p for p in ['c', 'd', 'e', 'f', 'g', 'h']]
-  blobs = np.swapaxes(blobs, 0, 1)
-  blobs = blobs[:,(nburn // thin):,:].reshape(nwalk * (nsteps - nburn) // thin, -1)
-  blobs *= 180 / np.pi
-  fig = corner.corner(blobs, labels = labels, range = [(0, 2)] + [1 for j in range(len(labels) - 1)])
-  fig.set_size_inches(9,9)
-  
-  # Appearance tweaks
-  for ax in fig.axes:
-    for tick in ax.get_xticklabels() + ax.get_yticklabels():
-      tick.set_fontsize(8)
-    ax.set_ylabel(ax.get_ylabel(), fontsize = 14, fontweight = 'bold', labelpad = 35)
-    ax.set_xlabel(ax.get_xlabel(), fontsize = 14, fontweight = 'bold', labelpad = 35)
-
-  # Show
-  fig.savefig('omega.pdf', bbox_inches = 'tight')
-
 def Trappist1(sample = True, **kwargs):
   '''
   
@@ -213,14 +77,12 @@ def Trappist1(sample = True, **kwargs):
       inc = 180 - inc
     
     # Longitude of ascending node in degrees
-    # This is computed from a distribution with
-    # standard deviation 0.4 degrees, which we
-    # obtain in `OmegaPrior()` above. For
-    # definiteness and WLOG, we choose Omega_b = 0.
+    # A standard deviation of 0.3 is what Eric Agol got
+    # in his Monte Carlo runs
     if i == 0:
       Omega = 0
     else:
-      Omega = 0.4 * np.random.randn()
+      Omega = 0.3 * np.random.randn()
     
     # Longitude of pericenter (uniform over [0-360 deg])
     w = 360. * np.random.rand()
