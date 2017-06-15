28f267d8acddff7ff644e59d15c680e7abc25b3f
Rodrigo Luger <rodluger@gmail.com>
2017-06-15 09:53:39 -0700

    better c

--- planetplanet/photo/ppo.py
+++ planetplanet/photo/ppo.py
@@ -42,19 +42,7 @@ __all__ = ['Star', 'Planet', 'System']
 
 # Load the library
 try:
-  # This is a bit of a hack. The `librebound.so` shared library must be in the CWD
-  # in order for `libppo.so` to find it. Let's copy it over if necessary, load the
-  # library, then delete it.
-  '''
-  cwd = os.getcwd()
-  if cwd != os.path.dirname(os.path.abspath(__file__)):
-    shutil.copy(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'librebound.so'), cwd)
-  '''
   libppo = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libppo.so'))
-  '''
-  if cwd != os.path.dirname(os.path.abspath(__file__)):
-    os.remove(os.path.join(cwd, 'librebound.so'))
-  '''
 except:
   raise Exception("Can't find `libppo.so`; please run `make` to compile it.")
 
