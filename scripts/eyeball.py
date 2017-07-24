8d6fad64381770badd495d150dff0b4d60c171fb
Rodrigo Luger <rodluger@gmail.com>
2017-07-24 14:39:12 -0700

    better occulted body plotting

--- scripts/eyeball.py
+++ scripts/eyeball.py
@@ -9,6 +9,7 @@ Interactive eyeball planet visualizer.
 '''
 
 import numpy as np
+np.seterr(invalid = 'ignore')
 import matplotlib.pyplot as pl
 from matplotlib.transforms import Affine2D
 import mpl_toolkits.axisartist.floating_axes as floating_axes
@@ -22,7 +23,8 @@ def ZenithColor(z):
   
   return pl.get_cmap('inferno')(0.3 + 0.3 * (np.cos(z) + 1))
 
-def PlotEyeball(theta = np.pi / 3, nz = 11, dphi = 0, dlam = 0, fig = None):
+def DrawEyeball(x0 = 0, y0 = 0, r = 1, theta = np.pi / 3, nz = 11, dphi = 0, dlam = 0, 
+                occultors = [], fig = None):
   '''
 
   '''
@@ -36,20 +38,20 @@ def PlotEyeball(theta = np.pi / 3, nz = 11, dphi = 0, dlam = 0, fig = None):
   
   # Compute the new effective theta 
   if theta + dphi < -np.pi:
-    theta = 2 * np.pi - np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
+    theta_eff = 2 * np.pi - np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
   elif theta + dphi < 0:
-    theta = -np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
+    theta_eff = -np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
   elif theta + dphi > np.pi:
-    theta = -np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
+    theta_eff = -np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
   else:
-    theta = np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
+    theta_eff = np.arccos(-x_ss * np.cos(alpha) + y_ss * np.sin(alpha))
   
   # Set up the floating axis
   if fig is None:
     fig = pl.figure(figsize = (6,6))
   tr = Affine2D().rotate_deg(-alpha * 180 / np.pi)
   x = 1. / (np.abs(np.cos(alpha)) + np.abs(np.sin(alpha)))
-  grid_helper = floating_axes.GridHelperCurveLinear(tr, extremes=(-x, x, -x, x), tick_formatter1 = None, tick_formatter2 = None)
+  grid_helper = floating_axes.GridHelperCurveLinear(tr, extremes=(-x, x, -x, x))
   ax_orig = floating_axes.FloatingSubplot(fig, 111, grid_helper = grid_helper)
   ax_orig.axis["bottom"].set_visible(False)
   ax_orig.axis["top"].set_visible(False)
@@ -58,12 +60,27 @@ def PlotEyeball(theta = np.pi / 3, nz = 11, dphi = 0, dlam = 0, fig = None):
   fig.add_subplot(ax_orig)
   ax = ax_orig.get_aux_axes(tr)
   
+  # Plot the occultors. Note that we need to transform
+  # their position vectors since we're in a rotated frame.
+  for occultor in occultors:
+    xo = occultor['x']
+    yo = occultor['y']
+    ro = occultor['r']
+    zo = occultor.get('zorder', 1)
+    co = occultor.get('color', 'gray')
+    xo, yo = xo * np.cos(alpha) - yo * np.sin(alpha), \
+             yo * np.cos(alpha) + xo * np.sin(alpha)
+    x = np.linspace(-ro, ro, 1000)
+    y = np.sqrt(ro ** 2 - x ** 2)
+    ax.plot(xo + x, yo + y, color = 'k', zorder = zo, lw = 1)
+    ax.plot(xo + x, yo - y, color = 'k', zorder = zo, lw = 1)
+    ax.fill_between(xo + x, yo - y, yo + y, color = co, zorder = zo - 0.01, lw = 1)
+    
   # Plot the occulted body
-  r = 1
   x = np.linspace(-r, r, 1000)
   y = np.sqrt(r ** 2 - x ** 2)
-  ax.plot(x, y, color = 'k', zorder = 98, lw = 1)
-  ax.plot(x, -y, color = 'k', zorder = 98, lw = 1)
+  ax.plot(x0 + x, y0 + y, color = 'k', zorder = 0, lw = 1)
+  ax.plot(x0 + x, y0 - y, color = 'k', zorder = 0, lw = 1)
 
   # Plot the zenith angle ellipses
   zarr = np.linspace(0, np.pi, nz + 2)
@@ -71,52 +88,52 @@ def PlotEyeball(theta = np.pi / 3, nz = 11, dphi = 0, dlam = 0, fig = None):
 
     # The ellipse
     a = r * np.abs(np.sin(z))
-    b = max(0.001, a * np.abs(np.sin(theta)))
-    xE = -r * np.cos(z) * np.cos(theta)
-    xlimb = r * np.cos(z) * np.sin(theta) * np.tan(theta)
-    if ((theta > 0) and (b < xlimb)) or ((theta <= 0) and (b > xlimb)):
+    b = max(0.001, a * np.abs(np.sin(theta_eff)))
+    xE = -r * np.cos(z) * np.cos(theta_eff)
+    xlimb = r * np.cos(z) * np.sin(theta_eff) * np.tan(theta_eff)
+    if ((theta_eff > 0) and (b < xlimb)) or ((theta_eff <= 0) and (b > xlimb)):
       xmin = xE - b
     else:
       xmin = xE - xlimb
-    if ((theta > 0) and (b > -xlimb)) or ((theta <= 0) and (b < -xlimb)):
+    if ((theta_eff > 0) and (b > -xlimb)) or ((theta_eff <= 0) and (b < -xlimb)):
       xmax = xE + b
     else:
       xmax = xE - xlimb
         
     # Plot it
     x = np.linspace(xE - b, xE + b, 1000)
-    if theta > 0:
+    if theta_eff > 0:
       x[x < xE - xlimb] = np.nan
-    elif theta > -np.pi / 2:
+    elif theta_eff > -np.pi / 2:
       x[x > xE - xlimb] = np.nan
     A = b ** 2 - (x - xE) ** 2
     A[A < 0] = 0
     y = (a / b) * np.sqrt(A)
     if np.abs(np.cos(z)) < 1e-5:
-      style = dict(color = 'k', ls = '--', lw = 0.5, zorder = 9999)
+      style = dict(color = 'k', ls = '--', lw = 0.5, zorder = 0)
     else:
-      style = dict(color = 'k', ls = '-', lw = 0.5, zorder = 9999)
-    ax.plot(x, y, **style)
-    ax.plot(x, -y, **style)
+      style = dict(color = 'k', ls = '-', lw = 0.5, zorder = 0)
+    ax.plot(x0 + x, y0 + y, **style)
+    ax.plot(x0 + x, y0 - y, **style)
     
     # Fill the ellipses
-    if theta < 0:
-      ax.fill_between(x, -y, y, color = ZenithColor(z), zorder = int(100 * z))
+    if theta_eff < 0:
+      ax.fill_between(x0 + x, y0 - y, y0 + y, color = ZenithColor(zarr[i+1]), zorder = 0.5 * (z / np.pi - 1))
     else:
-      ax.fill_between(x, -y, y, color = ZenithColor(z), zorder = int(-100 * z))
+      ax.fill_between(x0 + x, y0 - y, y0 + y, color = ZenithColor(zarr[i]), zorder = 0.5 * (-z / np.pi - 1))
   
     # Fill the ellipses that are cut off by the limb
-    if theta < 0:
+    if theta_eff < 0:
       x_ = np.linspace(-r, xE - xlimb, 1000)
       y_ = np.sqrt(r ** 2 - x_ ** 2)
-      ax.fill_between(x_, -y_, y_, color = ZenithColor(zarr[i]), zorder = int(-100 * z))
+      ax.fill_between(x0 + x_, y0 - y_, y0 + y_, color = ZenithColor(zarr[i]), zorder = 0.5 * (-z / np.pi - 1))
     else:
       x_ = np.linspace(-r, xE - xlimb, 1000)
       y_ = np.sqrt(r ** 2 - x_ ** 2)
-      ax.fill_between(x_, -y_, y_, color = ZenithColor(z), zorder = int(-100 * z))
-    
-  return fig, ax_orig, ax
-
+      ax.fill_between(x0 + x_, y0 - y_, y0 + y_, color = ZenithColor(zarr[i]), zorder = 0.5 * (-z / np.pi - 1))
+  
+  return fig, ax
+  
 class Interact(object):
   '''
   
@@ -152,7 +169,9 @@ class Interact(object):
         ax.remove()
   
     # Plot the planet
-    PlotEyeball(fig = self.fig, theta = self.theta.val * np.pi / 180, dphi = self.psi.val * np.pi / 180, dlam = self.lam.val * np.pi / 180)
+    DrawEyeball(fig = self.fig, theta = self.theta.val * np.pi / 180, dphi = self.psi.val * np.pi / 180, dlam = self.lam.val * np.pi / 180)
 
 if __name__ == '__main__':
+  
   Interact()
+  
