5195b8036f12827aa8455d1de48e2807f526bcc0
Rodrigo Luger <rodluger@gmail.com>
2017-07-28 11:28:40 -0700

    working

--- scripts/exomoon.py
+++ scripts/exomoon.py
@@ -24,7 +24,7 @@ star = Star('A', m = 0.1, r = 0.1, nz = 31, color = 'k', limbdark = [0.4, 0.26])
 # Planet b
 b = Planet('b', m = 50., per = 1, inc = 90., r = 1., t0 = 0, 
            nz = 11, Omega = 0, w = 0., ecc = 0., phasecurve = True, color = 'r')
-
+           
 # Moon
 m = Moon('bI', 'b', m = 0., per = 0.1, inc = 90., r = 0.5, t0 = 0.5, 
          nz = 11, Omega = 10, w = 0., ecc = 0., phasecurve = True, color = 'b')
