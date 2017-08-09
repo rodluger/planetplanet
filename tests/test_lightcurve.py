c0c890ab51e3599297afe37c7bd808855381d176
Rodrigo Luger <rodluger@gmail.com>
2017-08-08 21:07:15 -0700

    updated tests

--- tests/test_lightcurve.py
+++ tests/test_lightcurve.py
@@ -9,12 +9,8 @@ Test the occultation code.
 '''
 
 from planetplanet import Star, Planet, System
+from planetplanet.constants import *
 import numpy as np
-TOL = 0.001
-RSUN = 6.957e8
-PARSEC = 3.086e16
-MICRON = 1.e-6
-SBOLTZ = 5.670367e-8
 
 def u1(lam):
   '''
@@ -29,7 +25,7 @@ def u1(lam):
   result[lam > 15] = 0.5
   return result
 
-def test_mutual():
+def test_mutual(tol = 1e-10):
   '''
   
   '''
@@ -53,10 +49,10 @@ def test_mutual():
   system.compute(time)
   
   # Benchmarked values
-  truths = [7.6963687220060673e-15,
-            1.1495664454458829e-16,
-            8.5930679511725155e-12,
-            1.247748695405246e-13]
+  truths = [7.69636872134e-15,
+            1.14956644064e-16,
+            8.59304867308e-12,
+            1.24774758808e-13]
   
   # Computed values
   values = [system.flux[500,0],
@@ -65,16 +61,62 @@ def test_mutual():
             np.sum(system.flux[:,-1])]
 
   # Check!
-  assert np.abs(values[0] - truths[0]) / truths[0] < TOL, "Incorrect flux."
-  assert np.abs(values[1] - truths[1]) / truths[1] < TOL, "Incorrect flux."
-  assert np.abs(values[2] - truths[2]) / truths[2] < TOL, "Incorrect average flux."
-  assert np.abs(values[3] - truths[3]) / truths[3] < TOL, "Incorrect average flux."
+  assert np.abs(values[0] - truths[0]) / truths[0] < tol, "Incorrect flux: %.10e != %.10e." % (values[0], truths[0])
+  assert np.abs(values[1] - truths[1]) / truths[1] < tol, "Incorrect flux: %.10e != %.10e." % (values[1], truths[1])
+  assert np.abs(values[2] - truths[2]) / truths[2] < tol, "Incorrect average flux: %.10e != %.10e." % (values[2], truths[2])
+  assert np.abs(values[3] - truths[3]) / truths[3] < tol, "Incorrect average flux: %.10e != %.10e." % (values[3], truths[3])
 
-def test_limbdark():
+def test_compare_methods(tol = 1e-6):
   '''
   
   '''
+
+  # Instantiate the star
+  star = Star('star', m = 0.1, r = 0.1, nz = 31, color = 'k', limbdark = [0.4, 0.26])
+
+  # Planet b
+  planet = Planet('planet', m = 1, per = 0.5, inc = 90.4, r = 2., t0 = 0, 
+                  nz = 1, Omega = 0, w = 0., ecc = 0., phasecurve = False)
+
+  # Time array
+  time = np.arange(-0.025, 0.025, 0.1 * MINUTE)
   
+  # Compute the light curve, no optimization
+  system = System(star, planet, batmanopt = False, circleopt = False)
+  system.compute(time, lambda1 = 0.5, lambda2 = 2.)
+  flux1 = system.star.flux[:,0] / system.star.flux[0,0]
+  
+  # Compute the light curve w/ circle optimization
+  system = System(star, planet, batmanopt = False, circleopt = True)
+  system.compute(time, lambda1 = 0.5, lambda2 = 2.)
+  flux2 = system.star.flux[:,0] / system.star.flux[0,0]
+  
+  # Compute the light curve w/ batman optimization
+  system = System(star, planet, batmanopt = True, circleopt = False)
+  system.compute(time, lambda1 = 0.5, lambda2 = 2.)
+  flux3 = system.star.flux[:,0] / system.star.flux[0,0]
+  
+  # Flux minima
+  flux1min = flux1.min()
+  flux2min = flux2.min()
+  flux3min = flux3.min()
+  
+  # Flux averages
+  flux1mean = flux1.mean()
+  flux2mean = flux2.mean()
+  flux3mean = flux3.mean()
+  
+  # Check!
+  assert (flux1min - flux2min) / flux1min < tol, "Incorrect flux: %.10e != %.10e" % (flux1min, flux2min)
+  assert (flux1min - flux3min) / flux1min < tol, "Incorrect flux: %.10e != %.10e" % (flux1min, flux3min)
+  assert (flux1mean - flux2mean) / flux1mean < tol, "Incorrect average flux: %.10e != %.10e" % (flux1mean, flux2mean)
+  assert (flux1mean - flux3mean) / flux1mean < tol, "Incorrect average flux: %.10e != %.10e" % (flux1mean, flux3mean)
+  
+def test_limbdark(tol = 0.001):
+  '''
+  
+  '''
+    
   # Instantiate the star
   r = 0.1
   teff = 3200
@@ -96,8 +138,9 @@ def test_limbdark():
   lum = 4 * np.pi * bol * (12.2 * PARSEC) ** 2
   
   # Check!
-  assert np.abs(lum - truth) / truth < TOL, "Incorrect bolometric flux."
+  assert np.abs(lum - truth) / truth < tol, "Incorrect bolometric flux: %.10e != %.10e." % (lum, truth)
 
 if __name__ == '__main__':
   test_mutual()
   test_limbdark()
+  test_compare_methods()
