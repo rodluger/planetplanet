754ef33c85a0c4ae53de90b7fbec1fa45cac325f
Rodrigo Luger <rodluger@gmail.com>
2017-08-28 15:22:05 -0700

    non-interactive backend for tests

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -9,6 +9,8 @@ Test all of the scripts.
 '''
 
 from __future__ import division, print_function, absolute_import, unicode_literals
+import matplotlib.pyplot as pl
+pl.switch_backend('agg')
 import sys, os
 SCRIPTS = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'scripts')
 sys.path.insert(1, SCRIPTS)
@@ -23,6 +25,7 @@ def test_all():
   for script in dir(scripts):
     if not script.startswith('_'):    
       getattr(scripts, script)._test()
+      pl.close()
 
 if __name__ == '__main__':
   test_all()
