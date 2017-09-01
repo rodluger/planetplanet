425a96f58bc073f7bdbad25e561671979a599adb
Rodrigo Luger <rodluger@gmail.com>
2017-08-31 18:14:52 -0700

    Auto-check for updates: experimental

--- planetplanet/__init__.py
+++ planetplanet/__init__.py
@@ -8,6 +8,7 @@ import os, subprocess
 __version__ = "0.0.1"
 __author__ = "Rodrigo Luger (rodluger@uw.edu)"
 __copyright__ = "Copyright 2017 Rodrigo Luger"
+__url__ = "https://raw.githubusercontent.com/rodluger/planetplanet/master/planetplanet/__init__.py"
 
 # Was everest imported from setup.py?
 try:
@@ -18,7 +19,19 @@ except NameError:
 if not __PLANETPLANET_SETUP__:
     
   # Import stuff
-  from . import photo, detect
+  from . import photo, detect, constants
+  from .version import VersionCheck
   from .photo import *
   from .photo.trappist1 import *
   from .detect import *
+  
+  # Check for updates every tenth time
+  file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.counter')
+  with open(file, 'a+') as f:  
+    f.write('+')
+    f.seek(0)
+    counts = len(f.read())
+  if counts >= 10:
+    VersionCheck()
+    os.remove(file)
+  del file
