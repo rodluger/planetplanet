c9f323908aba540305991cbc5a0102e3511bf8ee
Rodrigo Luger <rodluger@gmail.com>
2017-10-01 09:33:19 -0700

    snr checks done

--- scripts/snr_check.py
+++ scripts/snr_check.py
@@ -9,14 +9,15 @@ in the 15 micron JWST MIRI filter. This is used for benchmarking the
 code. The benchmarked values are
 
   .. code-block:: python
-
-    b: SNR = 4.250
-    c: SNR = 2.752
-    d: SNR = 0.931
-    e: SNR = 0.863
-    f: SNR = 0.652
-    g: SNR = 0.502
-    h: SNR = 0.117
+    
+        planetplanet    analytic     ratio
+    b:     4.250          5.182      0.820
+    c:     2.752          3.406      0.808
+    d:     0.931          1.129      0.825
+    e:     0.863          1.039      0.830
+    f:     0.652          0.798      0.816
+    g:     0.502          0.622      0.806
+    h:     0.117          0.141      0.830
 
   .. role:: raw-html(raw)
      :format: html
@@ -38,21 +39,15 @@ def _test():
     
     '''
     
-    snrs = compute()
+    planetplanet_snr()
+    analytic_snr()
     
-def compute():
+def planetplanet_snr():
     '''
     Simulate an observation of a secondary eclipse of each of
-    the TRAPPIST-1 planets with JWST MIRI at 15 microns.
+    the TRAPPIST-1 planets with JWST MIRI at 15 microns and
+    compute the SNR.
     
-    .. plot::
-         :align: center
-         
-         from scripts import jwst_example
-         import matplotlib.pyplot as pl
-         jwst_example.Triple_bc()
-         pl.show()
-
     '''
     
     snrs = np.zeros(7)
@@ -63,7 +58,7 @@ def compute():
         # Instantiate the system, no random draw
         system = Trappist1(distance = 12, oversample = 30, nbody = False, 
                            sample = False, quiet = True)
-        
+
         # Get the body object
         body = system.bodies[planet + 1]
         
@@ -97,8 +92,83 @@ def compute():
         
     return snrs
 
+def analytic_snr():
+    '''
+    Compute the SNR analytically with back-of-the envelope
+    calculations. This is a sanity check on the values we get
+    with planetplanet.
+    
+    '''
+    
+    # Constants
+    PLANCK = 6.62606983e-27
+    GRAV = 6.67384e-8
+    BOLTZMANN = 1.3806488e-16
+    STEFANBOLTZMANN = 5.670373e-5
+    MHYDROGEN = 1.00794*1.66053886e-24
+    YEAR = 3.15569e7
+    C = 29979245800e0
+    AU = 1.49598e13
+    PC = 3.08568025e18
+    MSUN = 1.98892e33
+    RSUN = 6.955e10
+    MEARTH = 5.9742e27
+    REARTH = 6.3781e8
+    MMOON = 7.342e25
+    RMOON = 1737.1e5
+    MJUPITER = 1.8986e30
+    RJUPITER = 6.9911e9
+    AJUPITER = 4.84143144246472090 # AU
+    KILOGRAM = 1e3
+    METER = 1e2
+
+    # area of JWST in cm^2
+    area = 25.0 * 1e4  
+
+    # Star properties
+    teff = 2513.
+    rs = 0.121
+
+    # Eclipse duration in minutes
+    tec = np.array([36.4,42.37,49.13,57.21,62.6,68.4,76.7])
+
+    # Filter throughput
+    eps = 0.3
+
+    # Planet temperatures, zero albedo
+    tp = np.array([400.,342.,288.,251.,219.,199.,172.7])
+
+    # Planet radii, Earth radius
+    rp = np.array([1.086,1.056,0.772,0.918,1.045,1.127,0.755])
+
+    # Distance in parsecs
+    d = 12.0
+
+    # Filter wavelength, microns
+    lam = 15.0
+
+    # Filter width, microns
+    dlam = 4.0
+
+    # Compute the signal
+    nu = C / (1e-4 * lam)
+    dnu = dlam / lam * nu
+    x = PLANCK * nu / (BOLTZMANN * tp)
+    photons = 2. / (lam*1e-4) ** 2 / (np.exp(x) - 1.0) * np.pi * \
+              (rp * REARTH / d / PC) ** 2 * dnu * area * eps * tec * 60.
+
+    # Compute the noise
+    x = PLANCK * nu / (BOLTZMANN * teff)
+    photon_star = 2. / (lam * 1e-4) ** 2 / (np.exp(x) - 1.0) * np.pi * \
+                 (rs * RSUN / d / PC) ** 2 * dnu * area * eps * tec * 60.
+
+    # Compute the SNR
+    return photons / np.sqrt(photon_star)
+
 if __name__ == '__main__':
     
-    snrs = compute()
-    for planet, snr in zip(['b', 'c', 'd', 'e', 'f', 'g', 'h'], snrs):
-        print("%s: SNR = %.3f" % (planet, snr))
+    snrs1 = planetplanet_snr()
+    snrs2 = analytic_snr()
+    print("    planetplanet    analytic     ratio")
+    for planet, snr1, snr2 in zip(['b', 'c', 'd', 'e', 'f', 'g', 'h'], snrs1, snrs2):
+        print("%s:     %.3f          %.3f      %.3f" % (planet, snr1, snr2, snr1/snr2))
