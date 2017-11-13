362003dbe4bb607d5ad7d561f62949cb9553acf4
Rodrigo Luger <rodluger@gmail.com>
2017-11-13 13:45:38 -0800

    version bump [skip ci]

--- planetplanet/__init__.py
+++ planetplanet/__init__.py
@@ -6,7 +6,7 @@ from __future__ import division, print_function, absolute_import, \
 import os, subprocess
 
 # Version number
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 __author__ = "Rodrigo Luger (rodluger@uw.edu)"
 __copyright__ = "Copyright 2017 Rodrigo Luger"
 __url__ = "https://raw.githubusercontent.com/rodluger/planetplanet/master/planetplanet/__init__.py"
@@ -18,14 +18,14 @@ except NameError:
     __PLANETPLANET_SETUP__ = False
 
 if not __PLANETPLANET_SETUP__:
-        
+
     # Import stuff
     from . import photo, detect, constants, pool
     from .version import VersionCheck
     from .photo import *
     from .photo.trappist1 import *
     from .detect import *
-    
+
     # Check for updates?
     if not os.environ.get('PLANETPLANET_SKIP_VERSION_CHECK'):
         VersionCheck()
